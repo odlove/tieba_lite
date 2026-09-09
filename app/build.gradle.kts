001d2285a9470bdd789e8a55f380e1b86b9a56bf
v9pzm7 <44059332+odlove@users.noreply.github.com>
2026-09-09 13:18:40 +0800

    release: v0.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "app.tiebalite"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.0.2"
+        versionCode = 3
+        versionName = "0.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
